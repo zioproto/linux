51fb9f5ac1a9ea0c1d7491ee57b4750f04ab8236
Emanuele Delzeri <emanuele.delzeri@gmail.com>
2007-01-10 15:54:34 +0000

    Last version myhook.c
    git-svn-id: http://minerva.netgroup.uniroma2.it/svn/discreet/tfcproject/trunk/linux-2.6.16.19-TFC@27 0516eb0c-9f27-0410-bdae-aee6bd7d15d1

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -225,18 +225,17 @@ static int __xfrm_state_delete(struct xfrm_state *x)
 	del_timer(&x->tfc_alg_timer);
 	//printk(KERN_INFO "MAR _xfrm_state_delete\n");
 	//Svuoto la coda dei pacchetti
-	//printk(KERN_INFO "MAR xfrm_state_delete, qlen:%u\n",skb_queue_len(&x->tfc_list));
-	//while (!skb_queue_empty(&x->tfc_list)){
-	//	skb_dequeue(&x->tfc_list);
-	//	printk(KERN_INFO "MAR pacchetto rimosso\n");
-	//}
+	printk(KERN_INFO "MAR xfrm_state_delete, qlen:%u\n",skb_queue_len(&x->tfc_list));
+	while (!skb_queue_empty(&x->tfc_list)){
+		skb_dequeue(&x->tfc_list);
+		printk(KERN_INFO "MAR pacchetto rimosso\n");
+	}
 	//Svuoto la coda dei dummy
-	//printk(KERN_INFO "MAR xfrm_state_delete, dummy_qlen:%u\n",skb_queue_len(&x->dummy_list));
-	//while (!skb_queue_empty(&x->dummy_list)){
-	//	skb_dequeue(&x->dummy_list);
-	//	printk(KERN_INFO "MAR pacchetto dummy rimosso\n");
-	//}
-	//tfc_SA_remove(x);
+	printk(KERN_INFO "MAR xfrm_state_delete, dummy_qlen:%u\n",skb_queue_len(&x->dummy_list));
+	while (!skb_queue_empty(&x->dummy_list)){
+		skb_dequeue(&x->dummy_list);
+		printk(KERN_INFO "MAR pacchetto dummy rimosso\n");
+	}
 
 	int err = -ESRCH;
 
