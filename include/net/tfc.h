24f960b39e78644799ce0c61e892b446dbf1905d
Csaba Kiraly <kiraly@dit.unitn.it>
2007-03-26 15:00:24 +0000

    git-svn-id: http://minerva.netgroup.uniroma2.it/svn/discreet/tfcproject/trunk/linux-2.6.16.19-TFC@171 0516eb0c-9f27-0410-bdae-aee6bd7d15d1

--- include/net/tfc.h
+++ include/net/tfc.h
@@ -1,8 +1,14 @@
 #define TFC_ATTACH_PROTO IPPROTO_AH
 #define NEXTHDR_FRAGMENT_TFC 254
 
-void EspTfc_SA_init(struct xfrm_state *x);
+//fill the queue of dummy packets
+void build_dummy_pkt(struct xfrm_state *x);
+//add TFC header
+unsigned int tfc_apply(struct xfrm_state *x,struct sk_buff *skb);
+//start the logic (per SA)
+void SA_Logic(struct xfrm_state *x);
 
-
-struct sk_buff* tfc_input(struct sk_buff *skb);
-unsigned int tfc_defrag1(struct sk_buff *sb);
+//remove TFC header
+unsigned int tfc_remove(struct sk_buff *skb);
+//remove FAG header
+unsigned int tfc_defrag(struct sk_buff *skb);
